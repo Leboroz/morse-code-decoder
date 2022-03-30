8fc2f36a8b0c71df1bf41e5c98032715a0ee3859
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-30 14:29:00 -0600

    Create decode_char method

--- decoder.rb
+++ decoder.rb
@@ -27,10 +27,19 @@
   '--..' => 'Z'
 }
 
-
-
-def decode_char morse_char
+def decode_char(morse_char)
   @morse_code_char[morse_char]
 end
 
-puts decode_char ".-"
+def decode_word(word)
+  final_word = ''
+
+  word.split(/ /).each do |single|
+    final_word += decode_char(single)
+  end
+  final_word
+end
+
+puts decode_char '.-'
+
+puts decode_word('-- -.--')
